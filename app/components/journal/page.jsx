f07d5740b576314a9cccb5b71bd7fb7521d65ba3
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2023-07-28 18:05:12 +0530

    First

--- app/components/journal/page.jsx
+++ app/components/journal/page.jsx
@@ -14,9 +14,9 @@ const Journal = () => {
   return (
     <div>
       <div>
-        <div className="min-h-[90vh] lg:p-10 p-2 bg-[url('https://www.memonotepad.com/img/wood.png')] card shadow-5-strong backdrop-blur-[30px]">
+        <div className="min-h-[93vh] lg:p-10 p-2 bg-[url('https://www.memonotepad.com/img/wood.png')] card shadow-5-strong backdrop-blur-[30px]">
           <div className="min-h-[55vh] w-[60vw] bg-cover bg-no-repeat mx-auto rounded-xl p-2 bg-[url('https://img.freepik.com/premium-vector/paper-line_77417-515.jpg?size=626&ext=jpg')] card shadow-5-strong backdrop-blur-[30px]">
-            <div className={`text-center text-4xl mt-[30px] ${silkscreen.className}`}>
+            <div className={`text-center text-4xl mt-[60px] ${silkscreen.className}`}>
               My Journal
             </div>
             <textarea
@@ -24,10 +24,10 @@ const Journal = () => {
               onChange={(e) => {
                 setInputstate(e.target.value);
               }}
-              className=" leading-8 font-Caveat p-5 px-20 pt-[32px] text-lg mt-10 ml-10 bg-transparent outline-none rounded-xl w-full min-h-[55vh] h-full"
+              className="leading-8 font-Caveat p-5 px-28 pt-[32px] text-xl mt-10 ml-10 bg-transparent outline-none rounded-xl w-full min-h-[55vh] h-full"
             />
             {inputstate && (
-              <div className="bg-purple-600 ml-auto cursor-pointer hover:bg-purple-700 transition ease-in w-fit text-white py-2 px-4 rounded-lg">
+              <div className="bg-[#06D6A0] text-xl ml-auto cursor-pointer hover:bg-green-700 transition ease-in w-fit text-white py-2 px-4 rounded-lg">
                 Submit
               </div>
             )}
